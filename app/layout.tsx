e699641dd47bdaeed412061bafe736eda97c7007
JadenLR <jadenjanz@liveroom.dev>
2024-05-28 02:29:29 +0530

    add login user route and db conn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
 export const metadata: Metadata = {
   title: "DevSpace-HRM",
@@ -17,11 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <ColorSchemeScript />
-      </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import LoginClientPage from "./client-page";
+import { getDb } from "@/lib/database";
 
-export default function LoginPage() {
-  return <div>Login Page</div>;
+export default async function LoginPage() {
+  const db = await getDb();
+
+  return <LoginClientPage />;
 }
